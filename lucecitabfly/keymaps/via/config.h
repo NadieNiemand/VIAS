ee6b6031f7bf475aefc946c3a9d8893000f592a3
NadieNiemand <44422611+NadieNiemand@users.noreply.github.com>
2024-05-11 20:15:55 -0600

    LucecitaBfly

---
+++ lucecitabfly/keymaps/via/config.h
@@ -0,0 +1,14 @@
+
+#define RGB_DI_PIN GP8
+#define RGBLED_NUM 8
+#define RGBLIGHT_ANIMATIONS
+
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS                //Not defined Enable christmas animation mode.
+#define RGBLIGHT_EFFECT_KNIGHT                   //Not defined Enable knight animation mode.
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD             //Not defined Enable rainbow mood animation mode.
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL            //Not defined Enable rainbow swirl animation mode.
+#define RGBLIGHT_EFFECT_SNAKE                    //Not defined Enable snake animation mode.
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT          //Not defined Enable static gradient mode.
+#define RGBLIGHT_EFFECT_TWINKLE                  // Enable twinkle animation mode.
